4cb54431a15e6f926a2788e89d825d671044b898
mgracey <mgracey@hotmail.co.uk>
2024-12-01 11:11:47 +0000

    reconfig time chart

--- src/app/components/progress-chart/progress-chart.component.ts
+++ src/app/components/progress-chart/progress-chart.component.ts
@@ -19,39 +19,43 @@ export class ProgressChartComponent {
   chartOptions!: Highcharts.Options;
 
   constructor(private taskService: TaskService) {
-    
     this.taskService.getTasks$()
       .subscribe((tasks: Task[]) => {
         this.totalCompleted = tasks.filter((x)=> x.completed).length;
         this.totalIncomplete = tasks.filter((x)=> !x.completed).length;
-
-        let chartData: any;
-        let title: string;
-
-        if (this.totalCompleted || this.totalIncomplete) {
-          title = 'Completed vs Incomplete';
-          chartData = [
-            {name: 'Completed', y:this.totalCompleted},
-            {name: 'Incomplete', y:this.totalIncomplete}
-          ];
-          
-        }
-        else {
-          title = 'No task data is currently present';
-          chartData = [];
-        }
-
-        this.chartOptions = {
-          title: {
-            text: title
-          },
-          colors: [ "lightgreen", "#e34234"], 
-          series: [{
-            name: 'Tasks',
-            data: chartData,
-            type: 'pie'
-          }]
-        };
+        this.prepareChartData();
       });
+  }
+
+  private prepareChartData(): void {
+    let chartData: any;
+    let title: string;
+
+    if (this.totalCompleted || this.totalIncomplete) {
+      title = 'Completed vs Incomplete';
+      chartData = [
+        {name: 'Completed', y:this.totalCompleted},
+        {name: 'Incomplete', y:this.totalIncomplete}
+      ];
+    }
+    else {
+      title = 'No task data is currently present';
+      chartData = [];
+    }
+    this.setChartConfig(chartData, title);
+  }
+
+  private setChartConfig(chartData: any, title: string): void {
+    this.chartOptions = {
+      title: {
+        text: title
+      },
+      colors: [ "lightgreen", "#e34234"], 
+      series: [{
+        name: 'Tasks',
+        data: chartData,
+        type: 'pie'
+      }]
+    };
   }
 }
--- src/app/components/time-taken-chart/time-taken-chart.component.ts
+++ src/app/components/time-taken-chart/time-taken-chart.component.ts
@@ -17,49 +17,122 @@ export class TimeTakenChartComponent {
   chartOptions!: Highcharts.Options;
 
   constructor(private taskService: TaskService) {
-    
     this.taskService.getTasks$()
       .subscribe((tasks: Task[]) => {
-        const completedTasks = tasks.filter(task => task.completed);
-
-        let chartData: any = [];
-        let title: string; 
-
-        if (completedTasks.length) {
-          title = "Task completion times";
-
-          for(let task of completedTasks) {
-            let createDate = new Date(task.dateCreated).getTime();
-            let completeDate = new Date(task.dateCompleted || '').getTime();
-  
-            const takenMins = (completeDate - createDate) / (1000 * 60);
-            chartData.push([task.title, Math.round(takenMins)])
-          }
-        }
-        else {
-          title = "No completed task data is currently present"; 
-        }
-
-        this.chartOptions = {
-          title: {
-            text: title
-          },
-          series: [{
-            name: 'Time taken (in mins)',
-            data: chartData,
-            type: 'line'
-          }],
-          xAxis : {
-            labels :{
-              enabled: false
-            }
-          },
-          yAxis : {
-            title: {
-              text: 'Time (in mins)'
-            }
-          }
-        };
+        let completedTasks = tasks.filter(task => task.completed);
+        completedTasks = this.sortByCompletionTime(completedTasks);
+        this.prepareChartData(completedTasks);
       });
   }
+
+  private prepareChartData(completedTasks: Task[]): void {
+    let chartData: any = [];
+    let title: string; 
+
+    if (completedTasks.length) {
+      title = "Task completion times";
+
+      for(let task of completedTasks) {
+        let createDate = new Date(task.dateCreated).getTime();
+        let completeDate = new Date(task.dateCompleted || '').getTime();
+
+        const takenMins = (completeDate - createDate) / (1000 * 60);
+        chartData.push([task.title, Math.round(takenMins)])
+      }
+    }
+    else {
+      title = "No completed task data is currently present"; 
+    }
+
+    this.setChartConfig(chartData, title);
+
+  }
+
+  private setChartConfig(chartData: any, title: string): void {
+    this.chartOptions = {
+      title: {
+        text: title
+      },
+      tooltip: {
+        formatter: function () {              
+            return '<b>' + this.name + '</b><br>' + TimeTakenChartComponent.prototype.convertToTimeString(this.y || 0);
+        }
+      },
+      series: [{
+        name: 'Tasks',
+        data: chartData,
+        type: 'line'
+      }],
+      xAxis : {
+        title: {
+          text: 'Order of completion'
+        },
+        labels: {
+          formatter: function() {
+            let label = (typeof this.value === 'number') ? (this.value + 1).toString() : this.value;
+            return label
+          }
+        }
+      },
+      yAxis : {
+        title: {
+          text: 'Time taken'
+        },
+        labels :{
+          enabled: false
+        }
+      }
+    };
+  }
+
+  private sortByCompletionTime(tasks: Task[]): Task[] {
+    const sortedTasks = tasks.sort((a,b) => {
+      let fieldA = a.dateCompleted || '';
+      let fieldB = b.dateCompleted || '';
+
+      if (fieldA < fieldB) {
+        return -1;
+      }
+      if (fieldA > fieldB) {
+        return 1;
+      }
+      return 0;
+    });
+    
+    return sortedTasks;
+  }
+
+    /*
+    Times are measured in minutes so converting point these 
+    times into more readable measurements for point tooltips
+  */
+    private convertToTimeString(mins: number): string{
+      let timeString = [];
+      let days = 0;
+      let hours = 0;
+  
+      if (!mins) {
+        return 'less than a minute';
+      }
+  
+      //1440 mins in a day
+      if ((mins / 1440) >= 1) {
+        days = Math.floor(mins / 1440);
+        mins = mins % 1440;
+        timeString.push(days + (days === 1 ? ' day' : 'days'));
+      }
+  
+      //60 mins in a hour
+      if ((mins / 60) >= 1) {
+        hours = Math.floor(mins / 60);
+        mins = mins % 60;
+        timeString.push(hours + 'h');
+      }
+  
+      if (mins) {
+        timeString.push(mins + 'm');
+      }
+  
+      return timeString.join(',');
+    }
 }
